02ed6ab5891cd61926a05bbd3e2304c2cf8a671e
Kyle202210750 <133838256+Kyle202210750@users.noreply.github.com>
2023-09-29 22:45:43 +0800

    Create StringRushed1.java

---
+++ StringRushed1.java
@@ -0,0 +1,18 @@
+public class StringRushed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String givenString = "Kyle Alojado";
+        char indexChar1 = givenString.charAt(0);
+        char indexChar2 = givenString.charAt(5);
+
+        System.out.println("Character from given index1 is: " + indexChar1);
+        System.out.println("Character from given index2 is: " + indexChar2);
+        
+        // Output:
+        // Character from given index1 is: K
+        // Character from given index2 is: A
+    }
+}
